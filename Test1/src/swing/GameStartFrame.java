557b4b027a1cb5a325e0f4468ef8a93bb298eea4
김진렬 <>
2022-09-09 17:20:43 +0900

    🐥 날아오르라 주작이여

--- Test1/src/swing/GameStartFrame.java
+++ Test1/src/swing/GameStartFrame.java
@@ -1,6 +1,9 @@
 package swing;
 
 import javax.swing.*;
+
+import swing.Bird.BirdAni;
+import swing.Bird.BirdAni2;
 import swing.Char.*;
 import swing.Move.*;
 import swing.Skill.*;
@@ -15,7 +18,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GameStartFrame extends JFrame implements Runnable {
-
+    ImageIcon[] birdIcon;
     String nick = "bb";
     String imgPath;
     int FramW = 1000, FramH = 900, blockW = 100, blockH = 50, blockX = 450, blockY = 500,
@@ -49,8 +52,8 @@ public class GameStartFrame extends JFrame implements Runnable {
     int otherCharW, otherCharH, otherCharX, otherCharY;
     public ImageIcon[] otherCharArr, otherCharDown;
     public int otherCharIdx;
-    
-    int betweenStep=0;
+
+    int betweenStep = 0;
 
     public GameStartFrame(int charIdx, int otherCharIdx) {
         super("J프레임 테스트"); // 프레임의 타이틀
@@ -69,10 +72,6 @@ public class GameStartFrame extends JFrame implements Runnable {
         JPanel backPanel = new JPanel();
         backPanel.setLayout(null);
 
-
-
-        
-        
         // 3-2-1-go
         ImageIcon[] ImgArr3 = new ImageIcon[10];
         ImageIcon[] ImgArr2 = new ImageIcon[10];
@@ -84,10 +83,12 @@ public class GameStartFrame extends JFrame implements Runnable {
             ImgArr1[i] = imgMk("count/1.png", gameStartCountW - 100 * i, gameStartCountH - 40 * i);
             ImgArrGo[i] = imgMk("count/go.png", gameStartCountW - 100 * i, gameStartCountH - 40 * i);
         }
+
         JLabel jl3 = new JLabel(ImgArr3[0]);
         JLabel jl2 = new JLabel(ImgArr2[0]);
         JLabel jl1 = new JLabel(ImgArr1[0]);
         JLabel jlGo = new JLabel(ImgArrGo[0]);
+
         jl3.setBounds(0, 0, gameStartCountW, gameStartCountH);
         jl2.setBounds(0, 0, gameStartCountW, gameStartCountH);
         jl1.setBounds(0, 0, gameStartCountW, gameStartCountH);
@@ -101,6 +102,25 @@ public class GameStartFrame extends JFrame implements Runnable {
         jl2.setVisible(false);
         jl1.setVisible(false);
         jlGo.setVisible(false);
+
+        // 새
+        birdIcon = new ImageIcon[2];
+        birdIcon[0] = new ImageIcon(new ImageIcon(imgPath +
+                "bird/rbird.gif").getImage().getScaledInstance(60, 60, 0));
+        birdIcon[1] = new ImageIcon(new ImageIcon(imgPath +
+                "bird/lbird.gif").getImage().getScaledInstance(50, 50, 0));
+        JLabel[] birdJLabel = new JLabel[2];
+
+        birdJLabel[0] = new JLabel(birdIcon[0]);
+        birdJLabel[1] = new JLabel(birdIcon[1]);
+
+        birdJLabel[0].setBounds(-100, 100, 60, 60);
+        birdJLabel[1].setBounds(1100, 230, 50, 50);
+
+        birdJLabel[0].setVisible(false);
+        birdJLabel[1].setVisible(false);
+        backPanel.add(birdJLabel[0]);
+        backPanel.add(birdJLabel[1]);
 
         // 캐릭터
         ImageIcon[] charArr = new ImageIcon[12];
@@ -135,24 +155,26 @@ public class GameStartFrame extends JFrame implements Runnable {
             }
         }
 
-        //상대가 위에 있아래 있는지 보여주는 화살표
-        ImageIcon upCheckIcon=imgMk("sub/upCheck.png", 40,30);
-        ImageIcon downCheckIcon=imgMk("sub/downCheck.png", 40,30);
-        JLabel upChecklbl=new JLabel(upCheckIcon);
-        JLabel downChecklbl=new JLabel(downCheckIcon);
-        JLabel betweenlbl=new JLabel(betweenStep+"");
+        // 상대가 위에 있아래 있는지 보여주는 화살표
+        ImageIcon upCheckIcon = imgMk("sub/upCheck.png", 40, 30);
+        ImageIcon downCheckIcon = imgMk("sub/downCheck.png", 40, 30);
+        JLabel upChecklbl = new JLabel(upCheckIcon);
+        JLabel downChecklbl = new JLabel(downCheckIcon);
+        JLabel betweenlbl = new JLabel(betweenStep + "");
         upChecklbl.setBounds(300, 10, 40, 30);
         downChecklbl.setBounds(300, 805, 40, 30);
         betweenlbl.setBounds(300, 20, 70, 200);
         Font font = new Font("Gothic", Font.BOLD, betweenlbl.getFont().getSize() + 15);
+        // betweenlbl.setBackground(Color.YELLOW);
+        betweenlbl.setForeground(Color.orange);
         betweenlbl.setFont(font);
         backPanel.add(upChecklbl);
         backPanel.add(downChecklbl);
-        backPanel.add(betweenlbl);  
+        backPanel.add(betweenlbl);
         upChecklbl.setVisible(false);
         downChecklbl.setVisible(false);
         betweenlbl.setVisible(false);
-        
+
         // 게이지바5
         gaugeBar = new JProgressBar();
         gaugeBar.setValue(gauge);
@@ -201,7 +223,7 @@ public class GameStartFrame extends JFrame implements Runnable {
 
         // 스킬 블랙아이
         ImageIcon blackEyeIcon = imgMk("blackEye3.png", FramW, FramH);
- 
+
         blackEyelbl = new JLabel(blackEyeIcon);
         blackEyelbl.setBounds(0, 0, FramW, FramH);
         blackEyelbl.setVisible(false);
@@ -234,17 +256,16 @@ public class GameStartFrame extends JFrame implements Runnable {
 
         backPanel.setSize(FramW, FramH);
 
-
         // 초기 위치
 
         charlbl.setBounds(charX, charY, charW, charW);
         otherCharlbl.setBounds(otherCharX, otherCharY, otherCharW, otherCharW);
         // 배경
-        ImageIcon [] backgroundIcon=new ImageIcon[9];
-        for(int i=0;i<backgroundIcon.length;i++) {
-        	 backgroundIcon[i] = imgMk("back/backg"+i+".png", FramW, 5000);
+        ImageIcon[] backgroundIcon = new ImageIcon[9];
+        for (int i = 0; i < backgroundIcon.length; i++) {
+            backgroundIcon[i] = imgMk("back/backg" + i + ".png", FramW, 5000);
         }
-       
+
         JLabel backlbl = new JLabel(backgroundIcon[0]);
         backPanel.add(backlbl);
         backlbl.setBounds(0, startBackH, FramW, 5000);
@@ -280,6 +301,13 @@ public class GameStartFrame extends JFrame implements Runnable {
 
                                 moving(backlbl, blockArr, charlbl, charArr, gaugeBar, stepsJL2, comboJL2);
                                 send(0);
+                                if (keyCount == 1) {
+                                    birdJLabel[0].setVisible(true);
+                                    birdJLabel[1].setVisible(true);
+                                    new BirdAni(birdJLabel, birdIcon).start();
+
+                                }
+                                new BirdAni2(birdJLabel, birdIcon, moveX).start();
                             }
                             break;
 
@@ -298,6 +326,12 @@ public class GameStartFrame extends JFrame implements Runnable {
                                     } else {
                                         gaugeUp(gaugeBar, gauge = 0);
                                         send(charIdx + 1);
+                                        if (keyCount == 1) {
+                                            birdJLabel[0].setVisible(true);
+                                            birdJLabel[1].setVisible(true);
+                                            new BirdAni(birdJLabel, birdIcon).start();
+                                        }
+                                        new BirdAni2(birdJLabel, birdIcon, moveX).start();
                                     }
                                     // new SkillIce(iceBackbl).start();
                                 }
@@ -340,33 +374,30 @@ public class GameStartFrame extends JFrame implements Runnable {
             new GaugeDown().start();
             new BackAni(backlbl, backgroundIcon).start();
 
-       
-
             while (gameRunning) {
                 otherMove();
                 timelbl.setText(timerCount.getTime());
-                otherCheck(otherCharlbl, upChecklbl, downChecklbl,betweenlbl);
+                otherCheck(otherCharlbl, upChecklbl, downChecklbl, betweenlbl);
                 gaugeUp(gaugeBar, gauge);
                 Thread.sleep(180);
 
             }
-  
-            	if(gameRunning==false) {
+
+            if (gameRunning == false) {
                 gameRunning = true;
                 try {
-                
-                	InfoDTO dto = new InfoDTO();
+
+                    InfoDTO dto = new InfoDTO();
                     dto.setCommand(Info.EXIT);
                     writer.writeObject(dto); // 역슬러쉬가 필요가 없음
-                    writer.flush();    
-
+                    writer.flush();
 
                 } catch (IOException e1) {
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
                 }
 
-            	}
+            }
 
         } catch (InterruptedException e1) {
 
@@ -420,17 +451,15 @@ public class GameStartFrame extends JFrame implements Runnable {
             if (gauge < 100 && gauge > 0)
                 gaugeUp(gaugeBar, gauge -= 6);
 
+            for (int i = 0; i < hplbl.length; i++) {
+                hplbl[i].setVisible(false);
 
-                for (int i = 0; i < hplbl.length; i++) {
-                    hplbl[i].setVisible(false);
-
-                }
-                for (int i = 0; i < hp; i++) {
-                    hplbl[i].setVisible(true);
-                }
-            }  // charIdx == 2거나 아닐때
-        }
-    
+            }
+            for (int i = 0; i < hp; i++) {
+                hplbl[i].setVisible(true);
+            }
+        } // charIdx == 2거나 아닐때
+    }
 
     // 캐릭터 움직이는 함수
     public void moving(JLabel backlbl, JLabel[] blockArr, JLabel charlbl, ImageIcon[] charArr, JProgressBar gaugeBar,
@@ -522,13 +551,13 @@ public class GameStartFrame extends JFrame implements Runnable {
             try {
                 dto = (InfoDTO) reader.readObject();
                 if (dto.getCommand() == Info.EXIT) { // 서버로부터 내 자신의 exit를 받으면 종료됨
-                	//reader.close();
-                	//writer.close();
-                	//socket.close();
-                    //dispose();
-                   // new GameStartFrame(charIdx, otherCharIdx);
-                	 System.exit(0);
-            
+                    // reader.close();
+                    // writer.close();
+                    // socket.close();
+                    // dispose();
+                    // new GameStartFrame(charIdx, otherCharIdx);
+                    System.exit(0);
+
                 } else if (dto.getCommand() == Info.SEND) {
                     if (dto.getNickName() != null && dto.getNickName().equals(otherNick)) {
                         otherKeyCount = dto.getStep();
@@ -555,8 +584,8 @@ public class GameStartFrame extends JFrame implements Runnable {
 
     public void otherMove() {
         otherCharlbl.setLocation(blockArr[otherKeyCount].getLocation().x + otherCharX - 450,
-        blockArr[otherKeyCount].getLocation().y + otherCharY - 500);
-        betweenStep=otherKeyCount-keyCount;
+                blockArr[otherKeyCount].getLocation().y + otherCharY - 500);
+        betweenStep = otherKeyCount - keyCount;
 
         // -20 -130
     }
@@ -565,50 +594,51 @@ public class GameStartFrame extends JFrame implements Runnable {
     public void gaugeUp(JProgressBar gaugeBar, int gauge) {
         gaugeBar.setValue(gauge);
     }
-    public void otherCheck(JLabel otherCharlbl,JLabel upChecklbl,JLabel downChecklbl,JLabel betweenlbl) {
-    	//-170 780
-    	
-    	if(-170<otherCharlbl.getLocation().y&&780>otherCharlbl.getLocation().y) {
-    		upChecklbl.setVisible(false);
-    		downChecklbl.setVisible(false);
-    		betweenlbl.setVisible(false);
-    	}else if(-170>=otherCharlbl.getLocation().y){
-    		betweenlbl.setVisible(true);
-    		upChecklbl.setVisible(true);
-    		downChecklbl.setVisible(false);
-    		
-    		betweenlbl.setText(betweenStep+"");
 
-    		if(0<otherCharlbl.getLocation().x+85&&otherCharlbl.getLocation().x+85<=935) {
-    			upChecklbl.setLocation(otherCharlbl.getLocation().x+80,upChecklbl.getLocation().y);
-        		betweenlbl.setLocation(otherCharlbl.getLocation().x+85,upChecklbl.getLocation().y-60);
-        	}else if(otherCharlbl.getLocation().x<=0){
-        		upChecklbl.setLocation(10,upChecklbl.getLocation().y);
-        		betweenlbl.setLocation(15,upChecklbl.getLocation().y-60);
-        	}else {
-        		upChecklbl.setLocation(950,upChecklbl.getLocation().y);
-        		betweenlbl.setLocation(951,upChecklbl.getLocation().y-60);
-        	}
-    //935
-    	}else {
-    		betweenlbl.setVisible(true);
-    		upChecklbl.setVisible(false);
-    		downChecklbl.setVisible(true);
-    		betweenlbl.setText(-betweenStep+"");
+    public void otherCheck(JLabel otherCharlbl, JLabel upChecklbl, JLabel downChecklbl, JLabel betweenlbl) {
+        // -170 780
 
-    	if(0<otherCharlbl.getLocation().x+85&&otherCharlbl.getLocation().x+85<=935) {
-    		downChecklbl.setLocation(otherCharlbl.getLocation().x+80,downChecklbl.getLocation().y);
-    		betweenlbl.setLocation(otherCharlbl.getLocation().x+85,downChecklbl.getLocation().y-115);
-    	}else if(otherCharlbl.getLocation().x<=0){
-    		downChecklbl.setLocation(10,downChecklbl.getLocation().y);
-    		betweenlbl.setLocation(15,downChecklbl.getLocation().y-115);
-    	}else {
-    		downChecklbl.setLocation(950,downChecklbl.getLocation().y);
-    		betweenlbl.setLocation(951,downChecklbl.getLocation().y-115);
-    	}
-//935
- //55
-    	}
+        if (-170 < otherCharlbl.getLocation().y && 780 > otherCharlbl.getLocation().y) {
+            upChecklbl.setVisible(false);
+            downChecklbl.setVisible(false);
+            betweenlbl.setVisible(false);
+        } else if (-170 >= otherCharlbl.getLocation().y) {
+            betweenlbl.setVisible(true);
+            upChecklbl.setVisible(true);
+            downChecklbl.setVisible(false);
+
+            betweenlbl.setText(betweenStep + "");
+
+            if (0 < otherCharlbl.getLocation().x + 85 && otherCharlbl.getLocation().x + 85 <= 935) {
+                upChecklbl.setLocation(otherCharlbl.getLocation().x + 80, upChecklbl.getLocation().y);
+                betweenlbl.setLocation(otherCharlbl.getLocation().x + 85, upChecklbl.getLocation().y - 60);
+            } else if (otherCharlbl.getLocation().x <= 0) {
+                upChecklbl.setLocation(10, upChecklbl.getLocation().y);
+                betweenlbl.setLocation(15, upChecklbl.getLocation().y - 60);
+            } else {
+                upChecklbl.setLocation(950, upChecklbl.getLocation().y);
+                betweenlbl.setLocation(951, upChecklbl.getLocation().y - 60);
+            }
+            // 935
+        } else {
+            betweenlbl.setVisible(true);
+            upChecklbl.setVisible(false);
+            downChecklbl.setVisible(true);
+            betweenlbl.setText(-betweenStep + "");
+
+            if (0 < otherCharlbl.getLocation().x + 85 && otherCharlbl.getLocation().x + 85 <= 935) {
+                downChecklbl.setLocation(otherCharlbl.getLocation().x + 80, downChecklbl.getLocation().y);
+                betweenlbl.setLocation(otherCharlbl.getLocation().x + 85, downChecklbl.getLocation().y - 115);
+            } else if (otherCharlbl.getLocation().x <= 0) {
+                downChecklbl.setLocation(10, downChecklbl.getLocation().y);
+                betweenlbl.setLocation(15, downChecklbl.getLocation().y - 115);
+            } else {
+                downChecklbl.setLocation(950, downChecklbl.getLocation().y);
+                betweenlbl.setLocation(951, downChecklbl.getLocation().y - 115);
+            }
+            // 935
+            // 55
+        }
     }
 
 }
--- Test1/src/swing/Setting.java
+++ Test1/src/swing/Setting.java
@@ -1,17 +1,19 @@
 package swing;
 
 public class Setting {
-    private String imgPath="src/img/"; //img까지 경로
-    private int blockCount=250; //생성할 블록 갯수
-    private int hp[]= {5,5,10}; //hp
-    private int gameTime= 100;//게임 진행시간
-    
-    private String charName[]={"snowChar","ghostChar","miraChar"};
-    private int charW[] = {180,130,100}, charH[] = {180,130,100} ,charX[] = {410,430,460}, charY[] = {330,375,410};
+    private String imgPath = "InfiniteStairs/Test1/src/img/"; // img까지 경로
+    private int blockCount = 250; // 생성할 블록 갯수
+    private int hp[] = { 5, 5, 10 }; // hp
+    private int gameTime = 100;// 게임 진행시간
 
-    public String getImgPath(){
+    private String charName[] = { "snowChar", "ghostChar", "miraChar" };
+    private int charW[] = { 180, 130, 100 }, charH[] = { 180, 130, 100 }, charX[] = { 410, 430, 460 },
+            charY[] = { 330, 375, 410 };
+
+    public String getImgPath() {
         return imgPath;
     }
+
     public int getBlockCount() {
         return blockCount;
     }
@@ -19,51 +21,65 @@ public class Setting {
     public void setBlockCount(int blockCount) {
         this.blockCount = blockCount;
     }
+
     public int[] getHp() {
         return hp;
     }
+
     public String[] getCharName() {
         return charName;
     }
+
     public void setCharName(String[] charName) {
         this.charName = charName;
     }
+
     public void setHp(int[] hp) {
         this.hp = hp;
     }
+
     public void setImgPath(String imgPath) {
         this.imgPath = imgPath;
     }
+
     public int getGameTime() {
         return gameTime;
     }
+
     public void setGameTime(int gameTime) {
         this.gameTime = gameTime;
     }
+
     public int[] getCharW() {
         return charW;
     }
+
     public void setCharW(int[] charW) {
         this.charW = charW;
     }
+
     public int[] getCharH() {
         return charH;
     }
+
     public void setCharH(int[] charH) {
         this.charH = charH;
     }
+
     public int[] getCharX() {
         return charX;
     }
+
     public void setCharX(int[] charX) {
         this.charX = charX;
     }
+
     public int[] getCharY() {
         return charY;
     }
+
     public void setCharY(int[] charY) {
         this.charY = charY;
     }
-    
-    
+
 }
